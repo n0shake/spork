6039e7f06595631f950d1072f98995c2b359e36c
Abhishek Banthia <chelsea1712@gmail.com>
2015-07-21 14:03:53 +0530

    Adding method documentation!

--- ABTextFieldAnimations/UITextField+ABTextFieldAnimations.h
+++ ABTextFieldAnimations/UITextField+ABTextFieldAnimations.h
@@ -21,13 +21,59 @@ typedef enum : NSUInteger {
 
 @interface UITextField (ABTextFieldAnimations)
 
+/**
+ *  Shakes a UITextField and executes a block on shake completion
+ *
+ *  @param completionHandler A block object which is called after shake
+ */
 - (void)shakeWithCompletionHandler:(void(^)())completionHandler;
+
+/**
+ *  Bounces a UITextField and executes a block on bounce completion
+ *
+ *  @param completionHandler A block object which is called after bounce
+ */
 - (void)bounceWithCompletionHandler:(void(^)())completionHandler;
+
+/**
+ *  Animates a UITextField to zero opacity
+ */
 - (void)animateOpacity;
+
+/**
+ *  Animates a UITextField to full opacity
+ */
 - (void)removeOpacity;
+
+/**
+ *  Performs implicit animation on UITextField's opacity attribute. Same as animateOpacity method
+ */
 - (void)performImplicitTransparencyAnimation;
+
+/**
+ *  Reset or change UITextField's text attribute with animation
+ *
+ *  @param replacementText Final replacement text
+ */
 - (void)animateReplacementWithText:(NSString *)replacementText;
+
+/**
+ *  Reset or change UITextField's text attribute with animation. Executes a block on completion
+ *
+ *  @param replacementText   Final replacement text
+ *  @param completionHandler A block object which is called after replacement is performed
+ */
+
 - (void)animateReplacementWithText:(NSString *)replacementText withCompletionHandler:(void(^)())completionHandler;
+
+/**
+ *  Reset or change UITextField's text attribute with animation. Customize animation types.
+ *
+ *  @param animationType     Type of animation to be performed. Check TypeAnimation enum for different values
+ *  @param duration          Duration of animation
+ *  @param replacementText   Final replacement text
+ *  @param completionHandler A block object which is called after replacement is performed
+ */
 - (void)replaceTextWithAnimationType:(TypeAnimation)animationType andDuration:(CFTimeInterval)duration andReplacementText:(NSString *)replacementText andCompletionHandler:(void(^)())completionHandler;
 
 @end
